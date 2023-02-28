05387e8b1b8c5718963e6a0e0fed3e24c38cb009
rudi <rudidankk@gmail.com>
2023-02-28 02:55:04 +0000

    Completed Instantiantion Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,9 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+
+        Object mySkater = new Object();
+
+        return mySkater;
     }
 }
